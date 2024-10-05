a2e6fc1cd7913842014c8bd26fce249ed5a25547
Gulia <gulia.isaeva.pls@gmail.com>
2024-10-05 22:35:18 +0300

    init: add imports from fs module and try to get path to file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import fs from 'fs/promises';
+import  path from "path";
+import { fileURLToPath } from 'url';
+const fileContent = "I am fresh and young"
+const __filename = fileURLToPath(import.meta.url);
+const __dirname  = path.dirname(__filename);
+const pathToFile = path.join(__dirname,'files','fresh.txt')
+console.log(pathToFile)
 const create = async () => {
-    // Write your code here 
-};
+try {
+    await fs.access(filePath);
+    throw new Error('FS operation failed')
+} catch (error) {
+    if(error.code ==="ENOENT"){
+console.log(fileContent)    }
+
+}};
 
 await create();
