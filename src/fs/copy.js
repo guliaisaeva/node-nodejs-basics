f8691ad4fcb5f339e55fcc94ddde0ec691a391b7
Gulia <gulia.isaeva.pls@gmail.com>
2024-10-06 00:31:00 +0300

    feat: add copy.js task implementation

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -3,14 +3,38 @@ import  path from "path";
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url)
-console.log(__filename)
 const __dirname = path.dirname(__filename);
-console.log(__dirname)
+//from
 const pathToFileFolder = path.join(__dirname,"files");
+//to
 const pathToFilesCopyFolder = path.join(__dirname, "files_copy");
 
 const copy = async () => {
-    // Write your code here
+    try {
+        await fs.access(pathToFileFolder);
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
+    try {
+        await fs.access(pathToFilesCopyFolder);
+        throw new Error('Fs operation failed:copy exists');
+    } catch (error) {
+        if (error.code !== "ENOENT") {
+            throw new Error('FS operation failed');
+        }
+    }
+
+   await fs.mkdir(pathToFilesCopyFolder);
+
+   const files = await fs.readdir(pathToFileFolder);
+
+   await Promise.all(files.map(async (file) => {
+    const sourceFile = path.join(pathToFileFolder, file);
+    const destinationFile = path.join(pathToFilesCopyFolder, file);
+    await fs.copyFile(sourceFile, destinationFile);
+}));
+console.log('Files copied successfully!They are inside files_copy folder');
+
 };
 
 await copy();
