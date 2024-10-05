6e66dee7840cb29ca267cfb5df8f19d6c387b6d3
Gulia <gulia.isaeva.pls@gmail.com>
2024-10-05 23:41:43 +0300

    init : start working  on task copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,16 @@
+import fs from 'fs/promises';
+import  path from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url)
+console.log(__filename)
+const __dirname = path.dirname(__filename);
+console.log(__dirname)
+const pathToFileFolder = path.join(__dirname,"files");
+const pathToFilesCopyFolder = path.join(__dirname, "files_copy");
+
 const copy = async () => {
-    // Write your code here 
+    // Write your code here
 };
 
 await copy();
