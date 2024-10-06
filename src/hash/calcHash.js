f32f65b28155a7d49c06b6d110a18ce913a824c0
Gulia <gulia.isaeva.pls@gmail.com>
2024-10-07 01:33:16 +0300

    feat: implememt hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,8 +1,25 @@
 import fs from 'fs';
 import crypto from 'crypto';
+import  path from "path";
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename);
+const pathToFile = path.join(__dirname,"files",'fileToCalculateHashFor.txt');
 const calculateHash = async () => {
-    // Write your code here
+    const hash =crypto.createHash('sha256');
+    const readStream = fs.createReadStream(pathToFile);
+
+    readStream.on('data',(chunk)=>{
+        hash.update(chunk);
+    })
+    readStream.on('end',()=>{
+        const result =hash.digest('hex');
+        console.log(`SHA-256 Hash: ${result}`)
+    })
+    readStream.on('error', (error) => {
+        console.error(`Error reading the file: ${error.message}`);
+    });
 };
 
 await calculateHash();
