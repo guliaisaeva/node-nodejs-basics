ec034017e70b58a6844a3678609e5780595d293d
Gulia <gulia.isaeva.pls@gmail.com>
2024-10-07 21:59:49 +0300

    init: start task worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,13 @@
+import { Worker } from 'worker_threads';
+import os from 'os';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const performCalculations = async () => {
-    // Write your code here
+    const numberOfCores = os.cpus().length;
+    console.log(numberOfCores)
 };
 
 await performCalculations();
