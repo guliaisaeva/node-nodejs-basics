52e36b8064f6aaa226300e01d006ea4e535a8523
Gulia <gulia.isaeva.pls@gmail.com>
2024-10-07 22:53:12 +0300

    feat: implement main.js task

--- src/wt/main.js
+++ src/wt/main.js
@@ -5,9 +5,42 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 const performCalculations = async () => {
     const numberOfCores = os.cpus().length;
-    console.log(numberOfCores)
+    const resulContainer =[];
+    const workersContainer =[];
+
+    for (let i= 0; i<numberOfCores; i++){
+        const worker = new Worker(path.join(__dirname, 'worker.js'), { type: 'module' });
+        workersContainer.push(worker);
+        const numberToSend = 10 + i;
+        console.log(`Sending number ${numberToSend} to worker ${i}`);
+        worker.postMessage(numberToSend);
+
+        worker.on('message', (message) => {
+            resulContainer[i] = message;
+
+            if (resulContainer.length === numberOfCores && !resulContainer.includes(undefined)) {
+                console.log(resulContainer);
+            }
+        });
+
+        worker.on('error', () => {
+            resulContainer[i] = { status: 'error', data: null };
+            if (resulContainer.length === numberOfCores && !resulContainer.includes(undefined)) {
+                console.log(resulContainer);
+            }
+        });
+
+        worker.on('exit', (code) => {
+            if (code !== 0) {
+                console.error(`Worker stopped with exit code ${code}`);
+            }
+        });
+
+    }
 };
 
 await performCalculations();
+
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -3,18 +3,13 @@ import { parentPort } from 'worker_threads';
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
-const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
-    const sendToMain =(n)=>{
-        const result = nthFibonacci(n);
-        parentPort.postMessage({status:"resolved",data:result})
-    }
-};
+
 parentPort.on('message', (n) => {
     try {
-        sendResult(n);
-    } catch (error) {
-        parentPort.postMessage({ status: 'error', data: null });
-    }
+        const result = nthFibonacci(n);
+        parentPort.postMessage({ status: 'resolved', data: result });}
+        catch (error) {
+            console.error(`Worker error: ${error.message}`);
+            parentPort.postMessage({ status: 'error', data: null }); }
+
 });
-sendResult();
