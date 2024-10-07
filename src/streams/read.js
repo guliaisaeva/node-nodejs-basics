0acbe99f25d604e009fa6d242246fcd7b2324374
Gulia <gulia.isaeva.pls@gmail.com>
2024-10-07 11:52:21 +0300

    feat: implement streams read.js task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import { createReadStream } from 'fs';
+import path from 'path';
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const filePath = path.join(process.cwd(), 'src/streams/files/fileToRead.txt');
+    const readStream = createReadStream(filePath, { encoding: 'utf8' });
+
+    readStream.on('data', (chunk) => {
+        process.stdout.write(chunk);
+      });
+  } catch (error) {
+    console.error(`Error reading file: ${error.message}`);
+  }
 };
 
 await read();
+
