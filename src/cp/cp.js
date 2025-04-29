b972108e9543e851cd064c8a0f4cc11ebbe56288
Gulia <gulia.isaeva.pls@gmail.com>
2025-04-29 06:40:18 +0300

    feat: add child processes

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,26 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
-};
+import { spawn } from "child_process";
 
+const spawnChildProcess = async (args) => {
+  // Write your code here
+  const child = spawn("node", ["src/cp/files/script.js", ...args], {
+    stdio: ["pipe", "pipe", "inherit"],
+  });
+
+  process.stdin.on("data", (chunk) => {
+    if (child.stdin.writable) {
+      child.stdin.write(chunk);
+    }
+  });
+  child.stdout.on("data", (data) => {
+    console.log(`Received from child process: ${data}`);
+  });
+
+  child.on("exit", (code) => {
+    console.log(`Child process exited with code ${code}`);
+  });
+};
+const args = process.argv.slice(2);
+console.log(`Total number of arguments is ${args.length}`);
+console.log(`Arguments: ${JSON.stringify(args)}`);
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(args);
