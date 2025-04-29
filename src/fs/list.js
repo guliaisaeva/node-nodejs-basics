579822747542cf7d1d1ca6974377d2bf0e8662e9
Gulia <gulia.isaeva.pls@gmail.com>
2025-04-29 06:12:39 +0300

    feat: add list and read functionality

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,21 @@
+import fs from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const pathToFilesFolder = path.join(__dirname, "files");
 const list = async () => {
-    // Write your code here 
+  // Write your code here
+  try {
+    await fs.access(pathToFilesFolder);
+  } catch (error) {
+    throw new Error("Fs operation failed");
+  }
+
+  const filesFolderContent = await fs.readdir(pathToFilesFolder);
+  console.log(filesFolderContent);
+  filesFolderContent.forEach((file) => console.log(file));
 };
 
 await list();
